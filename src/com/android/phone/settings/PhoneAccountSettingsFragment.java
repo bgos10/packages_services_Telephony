6e0f548c46139f19e5e86b840ece3c0a844c555e
Danny Baumann <dannybaumann@web.de>
2015-12-02 16:31:18 +0100

    Revert "TeleService: Add new feature to vibrate when MO call accepted"
    We have a better solution for this.
    
    This reverts commit 7c444ef869f28f8c94fb7632267d8c2412950d17.
    
    Change-Id: I45aec1e5ce6d479605a4039d363a2e3b730025b4

--- src/com/android/phone/settings/PhoneAccountSettingsFragment.java
+++ src/com/android/phone/settings/PhoneAccountSettingsFragment.java
@@ -56,7 +56,6 @@ public class PhoneAccountSettingsFragment extends PreferenceFragment
     private static final String SIP_RECEIVE_CALLS_PREF_KEY = "sip_receive_calls_key";
 
     private static final String SHOW_DURATION_KEY = "duration_enable_key";
-    private static final String BUTTON_VIBRATE_CONNECTED_KEY = "button_vibrate_after_connected";
 
     private static final String LEGACY_ACTION_CONFIGURE_PHONE_ACCOUNT =
             "android.telecom.action.CONNECTION_SERVICE_CONFIGURE";
@@ -82,9 +81,8 @@ public class PhoneAccountSettingsFragment extends PreferenceFragment
     private ListPreference mUseSipCalling;
     private CheckBoxPreference mSipReceiveCallsPreference;
     private SipSharedPreferences mSipSharedPreferences;
-
-    private CheckBoxPreference mShowDurationCheckBox;
-    private CheckBoxPreference mVibrateAfterConnected;
+    
+    private SwitchPreference mShowDurationSwitch;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -212,14 +210,6 @@ public class PhoneAccountSettingsFragment extends PreferenceFragment
                     mShowDurationCheckBox.setSummary(checked ? R.string.duration_enable_summary
                             : R.string.duration_disable_summary);
         }
-
-        mVibrateAfterConnected = (CheckBoxPreference) findPreference(BUTTON_VIBRATE_CONNECTED_KEY);
-        if (mVibrateAfterConnected != null) {
-            mVibrateAfterConnected.setOnPreferenceChangeListener(this);
-            boolean checked = Settings.System.getInt(getContext().getContentResolver(),
-                    Constants.SETTINGS_VIBRATE_WHEN_ACCEPTED, 1) == 1;
-            mVibrateAfterConnected.setChecked(checked);
-        }
     }
 
     /**
@@ -251,11 +241,6 @@ public class PhoneAccountSettingsFragment extends PreferenceFragment
                     Constants.SETTINGS_SHOW_CALL_DURATION, checked ? 1 : 0);
             mShowDurationCheckBox.setSummary(checked ? R.string.duration_enable_summary
                     : R.string.duration_disable_summary);
-            return true;
-        } else if (pref == mVibrateAfterConnected) {
-            boolean doVibrate = (Boolean) objValue;
-            Settings.System.putInt(getContext().getContentResolver(),
-                    Constants.SETTINGS_VIBRATE_WHEN_ACCEPTED, doVibrate ? 1 : 0);
             return true;
         }
         return false;
